74c0eac74f8ca25b230bc38be07767ff8eb66da3
joost.hui@gmail.com <joost.hui@gmail.com>
2018-07-10 17:57:04 -0700

    Added an example experiment file to the repository.

---
+++ cmoea_example/cmoea_example.cpp
@@ -0,0 +1,265 @@
+// This example relies on the following other modules:
+// - misc
+// - nsgaext
+// - cmoea
+
+// Include standard
+#include <iostream>
+#include <fstream>
+
+// Include sferes
+#include <sferes/phen/parameters.hpp>
+#include <sferes/gen/evo_float.hpp>
+#include <sferes/eval/eval.hpp>
+#include <sferes/stat/best_fit.hpp>
+#include <sferes/eval/parallel.hpp>
+#include <sferes/modif/dummy.hpp>
+#include <sferes/modif/diversity.hpp>
+#include <sferes/run.hpp>
+#include <sferes/ea/dom_sort_basic.hpp>
+
+// Include modules
+#include <modules/cmoea/cmoea_util.hpp>
+#include <modules/cmoea/cmoea_nsga2.hpp>
+#include <modules/nsgaext/dom_sort_no_duplicates.hpp>
+#include <modules/misc/common_compare.hpp>
+
+// Include local
+#include <stat_cmoea.hpp>
+
+/* NAMESPACES */
+using namespace sferes;
+using namespace sferes::gen::evo_float;
+
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////* Parameters for the experiment *////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+/* Parameters for Sferes */
+struct Params {
+    
+    // Parameters for the vector we are evolving
+    struct evo_float {
+        SFERES_CONST float cross_rate = 0.1f;
+        SFERES_CONST float mutation_rate = 0.1f;
+        SFERES_CONST float eta_m = 15.0f;
+        SFERES_CONST float eta_c = 10.0f;
+        SFERES_CONST mutation_t mutation_type = polynomial;
+        SFERES_CONST cross_over_t cross_over_type = sbx;
+    };
+    
+    struct parameters {
+        SFERES_CONST float min = 0.0f;
+        SFERES_CONST float max = 1.0f;
+    };
+    
+    struct cmoea_nsga {
+        // Type of non-dominated comparison that will be used by CMOEA NSGA
+        typedef sferes::ea::_dom_sort_basic::non_dominated_f non_dom_f;
+    };
+
+    struct cmoea {
+        // Size of every CMOEA bin
+        static const unsigned bin_size = 10;
+        
+        // Number of CMOEA bins
+        static const unsigned nb_of_bins = 7; // = (2 ^ numberOfTasks - 1)
+        
+        // Where fitness objective will be stored before non-dominated sorting
+        static const unsigned obj_index = 0;
+    };
+    
+    /* Parameters for the population */
+    struct pop {
+        // Required by sferes, but ignored by CMOEA
+        SFERES_CONST unsigned size = 0;
+        
+        // The number of individuals create to initialize the population
+        SFERES_CONST unsigned init_size = 10;
+        
+        // The maximum number of individuals to try an create at the same time.
+        SFERES_CONST unsigned max_batch_size = 1024;
+
+        // The number of individuals created every generation
+        SFERES_CONST unsigned select_size = 100;
+        
+        // The number of generations for which to run the algorithm
+        SFERES_CONST unsigned nb_gen = 200;
+        
+//        // A multiplier on the number of individuals to create a generation 1.
+//        SFERES_CONST int initial_aleat = 1;
+
+        // Frequency at which to dump the archive
+        static const int dump_period = 200;
+        
+//        // Fequency at which to write a checkpoint
+//        static const int checkpoint_period = 10;
+    };
+
+    struct ea {
+    	typedef sferes::ea::dom_sort_basic_f dom_sort_f;
+    };
+    
+    struct stats {
+        static const size_t period = 1;
+    };
+};
+
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////* Fitness Function *////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+/**
+ * Object to calculate the fitness of an individual over a set of randomly
+ * generated mazes.
+ */
+SFERES_FITNESS(ExampleCmoeaFit, sferes::fit::Fitness) {
+public:
+    ExampleCmoeaFit(){
+        
+    }
+
+    // Calculates behavioral distance between individuals
+    template<typename Indiv>
+    float dist(Indiv& ind) const {
+        return compare::l1diff(_behavior, ind.fit().getBehavior());
+    }
+
+
+    // Evaluates the performance of an individual on all tasks
+    template<typename Indiv>
+    void eval(Indiv& ind) {
+    	for(size_t i=0; i<ind.size(); ++i){
+            _behavior.push_back(ind.data(i));
+    	}
+        float hard = ind.data(0) * ind.data(1) * ind.data(2);
+        float easy = ind.data(3);
+        float task0 = 0;
+        float task1 = 0;
+        float task2 = 0;
+        if(easy > hard){
+            task0 = easy;
+            task1 = (1-hard)/2.0;
+        } else {
+            task0 = easy;
+            task1 = hard;
+        }
+         if(hard > 0.9){
+             task2 = ind.data(4);
+         }
+        
+        _cmoea_task_performance.push_back(task0);
+        _cmoea_task_performance.push_back(task1);
+        _cmoea_task_performance.push_back(task2);
+        
+        cmoea::calculate_bin_fitness_mult(_cmoea_task_performance, _cmoea_bin_fitness);
+        this->_objs.push_back(0);
+        this->_objs.push_back(0);
+    }
+
+    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    //////////////* Fitness attributes *////////////////////
+    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version){
+        dbg::trace trace("fit", DBG_HERE);
+        sferes::fit::Fitness<Params,
+			typename stc::FindExact<ExampleCmoeaFit<Params, Exact>,
+			Exact>::ret>::serialize(ar, version);
+        ar & BOOST_SERIALIZATION_NVP(_behavior);
+        ar & BOOST_SERIALIZATION_NVP(_cmoea_task_performance);
+        ar & BOOST_SERIALIZATION_NVP(_cmoea_bin_fitness);
+        ar & BOOST_SERIALIZATION_NVP(_cmoea_bin_diversity);
+        ar & BOOST_SERIALIZATION_NVP(_divIndex);
+    }
+
+    // Getters and setters for CMOEA
+    std::vector<float> getBehavior() const { return _behavior;}
+    std::vector<float> &getBinFitnessVector(){return _cmoea_bin_fitness;}
+    float getBinFitness(size_t index){ return _cmoea_bin_fitness[index];}
+    std::vector<float> &getBinDiversityVector(){return _cmoea_bin_diversity;}
+    float getBinDiversity(size_t index){ return _cmoea_bin_diversity[index];}
+    float getCmoeaObj(size_t index){ return _cmoea_task_performance[index];}
+    
+    void initBinDiversity(){
+        if(_cmoea_bin_diversity.size() != Params::cmoea::nb_of_bins){
+            _cmoea_bin_diversity.resize(Params::cmoea::nb_of_bins);
+        }
+    }
+    
+    void setBinDiversity(size_t index, float div){
+        dbg::check_bounds(dbg::error, 0, index, _cmoea_bin_diversity.size(),
+        		DBG_HERE);
+        _cmoea_bin_diversity[index] = div;
+    }
+
+    
+
+protected:
+    // Vector of robot (end) points over all mazes
+    std::vector<float> _behavior;
+    // Performance on each task, as required for cmoea
+    std::vector<float> _cmoea_task_performance;
+    // Performance on each combination of tasks, as required for cmoea
+    std::vector<float> _cmoea_bin_fitness;
+    // The diversity score of an individual, as calculated by cmoea
+    std::vector<float> _cmoea_bin_diversity;
+    // The index of the diversity objective in the objective array
+    int _divIndex;
+};
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+//////////////////* Main Program *//////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+/* Sets up and runs the experiment. */
+int main(int argc, char **argv) {
+    std::cout << "Entering main: " << std::endl;
+    
+    time_t t = time(0) + ::getpid();
+    std::cout<<"seed: " << t << std::endl;
+    srand(t);
+    sferes::misc::seed(t);
+
+    /* Fitness function to use (a class, defined above), which makes use of the
+     * Params struct. */
+    typedef ExampleCmoeaFit<Params> fit_t;
+
+    // The genotype.
+    typedef gen::EvoFloat<5, Params> gen_t;
+    
+    // The phenotype
+    typedef phen::Parameters<gen_t, fit_t, Params> phen_t;
+
+    //Behavioral distance based only on current population.
+    typedef modif::Diversity<> mod_t;
+
+    // What statistics should be gathered
+    typedef boost::fusion::vector<stat::StatCmoea<phen_t, Params> > stat_t;
+
+    // The evaluator for the network
+    typedef eval::Eval<Params> eval_t;
+    
+    // CMOEA with NSGA-II non-dominated selection within each bin
+    typedef ea::CmoeaNsga2<phen_t, eval_t, stat_t, mod_t, Params> ea_t;
+
+    ea_t ea;
+    
+    run_ea(argc, argv, ea);
+
+    /* Record completion (makes it easy to check if the job was preempted). */
+    std::cout << "\n==================================" << \
+            "\n====Evolutionary Run Complete!====" << \
+            "\n==================================\n";
+    return 0;
+}
---
+++ cmoea_example/stat_cmoea.hpp
@@ -0,0 +1,144 @@
+/*
+ * stat_cmoea.hpp
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: Joost Huizinga
+ */
+
+#ifndef EXP_CMOEA_EXAMPLE_STAT_CMOEA_HPP_
+#define EXP_CMOEA_EXAMPLE_STAT_CMOEA_HPP_
+
+
+#include <numeric>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <sferes/stat/stat.hpp>
+
+#include <boost/graph/iteration_macros.hpp>
+#include <boost/spirit/include/karma_real.hpp>
+
+#include <sferes/dbg/dbg.hpp>
+
+#include <modules/continue/global_options.hpp>
+#include <modules/misc/common_compare.hpp>
+#include <modules/cmoea/cmoea_util.hpp>
+
+namespace sferes
+{
+namespace stat
+{
+
+SFERES_STAT(StatCmoea, Stat)
+{
+
+public:
+    StatCmoea(){
+        _first_line = true;
+    }
+    
+    static const size_t period = Params::stats::period;
+
+    typedef boost::shared_ptr<Phen> indiv_t;
+    typedef std::vector<indiv_t> pop_t;
+
+    template<typename E>
+    void refresh(const E& ea)
+    {
+        dbg::trace trace("stat", DBG_HERE);
+        typedef typename E::pop_t mixed_pop_t;
+
+        size_t categories = Params::cmoea::nb_of_bins;
+        size_t objectives = cmoea::getNrOfObjectives(categories);
+        size_t bin_size = Params::cmoea::bin_size;
+
+        dbg::out(dbg::info, "stat") << "Gen: " << ea.gen() << " period: " << period << std::endl;
+        if (ea.gen() % period != 0) return;
+
+        //Write header
+        dbg::out(dbg::info, "stat") << "Creating log file" << std::endl;
+        this->_create_log_file(ea, "innov_all_bin.dat");
+        dbg::out(dbg::info, "stat") << "Log file: " << this->_log_file << std::endl;
+        if(!this->_log_file) return;
+
+        if(this->firstLine()){
+            dbg::out(dbg::info, "stat") << "Writing header" << std::endl;
+            (*this->_log_file) << "#gen ";
+            for(size_t i=0; i<categories; ++i){
+                (*this->_log_file) << "obj_" << cmoea::getTaskIndicesStr(i, objectives) << " ";
+            }
+
+            (*this->_log_file) << "\n";
+            _first_line = false;
+        }
+
+        std::vector<size_t> best_fit_index(categories, 0);
+        std::vector<float> best_fit(categories, 0);
+        dbg::assertion(DBG_ASSERTION(categories == ea.archive().size()));
+
+        //Add them in this exact order so it is easy to put them back into the archive
+        _all.clear();
+        dbg::out(dbg::info, "stat") << "Archive size: " << ea.archive().size() << std::endl;
+        for(size_t i=0; i<ea.archive().size(); ++i){
+            dbg::check_bounds(dbg::error, 0, i, ea.archive().size(), DBG_HERE);
+            dbg::out(dbg::info, "stat") << "Bin: " << i << " size: " << ea.archive()[i].size() << std::endl;
+
+            best_fit[i]=ea.archive()[i][0]->fit().getBinFitness(i);
+            for(size_t j=0; j<ea.archive()[i].size(); ++j){
+                dbg::check_bounds(dbg::error, 0, j, ea.archive()[i].size(), DBG_HERE);
+                float fitness = ea.archive()[i][j]->fit().getBinFitness(i);
+                if(fitness > best_fit[i]){
+                    dbg::check_bounds(dbg::error, 0, i, best_fit.size(), DBG_HERE);
+                    dbg::check_bounds(dbg::error, 0, i, best_fit_index.size(), DBG_HERE);
+                    best_fit[i] = fitness;
+                    best_fit_index[i] = j;
+                }
+                _all.push_back(ea.archive()[i][j]);
+            }
+        }
+
+        dbg::assertion(DBG_ASSERTION(_all.size() == bin_size*categories));
+        dbg::check_bounds(dbg::error, 0, 0, ea.archive().size(), DBG_HERE);
+        dbg::check_bounds(dbg::error, 0, 0, best_fit_index.size(), DBG_HERE);
+        dbg::check_bounds(dbg::error, 0, best_fit_index[0], ea.archive()[0].size(), DBG_HERE);
+        _best_individual = ea.archive()[0][best_fit_index[0]];
+
+        
+        
+        //Start writing
+        dbg::out(dbg::info, "stat") << "Writing line" << std::endl;
+        (*this->_log_file) << ea.gen() << " ";                // generation
+        for(size_t i=0; i<best_fit.size(); ++i){
+            (*this->_log_file) << best_fit[i] << " ";
+        }
+        (*this->_log_file) << std::endl;
+        
+        
+        std::cout << "Genenration: " << ea.gen() << " combined performance: " << best_fit [0] << std::endl;
+    }
+
+    bool firstLine(){
+      return _first_line;
+    }
+
+    pop_t& getPopulation(){
+        dbg::trace trace("stat", DBG_HERE);
+        return _all;
+    }
+
+    std::string name(){
+        return "StatCmoea";
+    }
+
+protected:
+    indiv_t _best_individual;
+    pop_t _all;
+    bool _first_line;
+};
+}
+}
+
+
+
+#endif /* EXP_CMOEA_EXAMPLE_STAT_CMOEA_HPP_ */
