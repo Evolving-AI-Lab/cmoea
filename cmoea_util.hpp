9afa8d97c0602506c87cc83268240298491d683a
Joost Huizinga <joost.hui@gmail.com>
2018-07-10 17:44:10 -0700

    Added two extra utility functions to the cmoea_util file.

--- cmoea_util.hpp
+++ cmoea_util.hpp
@@ -7,6 +7,9 @@
 
 #ifndef MODULES_CMOEA_CMOEA_UTIL_HPP_
 #define MODULES_CMOEA_CMOEA_UTIL_HPP_
+
+#include <modules/misc/common_compare.hpp>
+
 namespace sferes {
 namespace cmoea {
 
@@ -44,6 +47,41 @@ void calculate_bin_fitness_mult(const std::vector<float>& task_performance, std:
         dbg::out(dbg::info, "binfit") << "obj: " << i << " fitness " << local_fitness << std::endl;
         bin_fitness.push_back(local_fitness);
     }
+}
+    
+// Not an efficient implementation, but it at least guarentees that the objectives returned here
+// are the objectives associated with that bin
+std::vector<int> getTaskIndices(int bin_index, int nr_of_tasks){
+    std::vector<int> result;
+    unsigned nr_of_bins = pow(2, nr_of_tasks) - 1;
+    for(unsigned j=0; j<nr_of_tasks; ++j){
+        if(int(bin_index/(1<<j))%2 == 0){
+            result.push_back(j);
+        }
+    }
+    return result;
+}
+    
+std::string getTaskIndicesStr(int bin_index, int nr_of_tasks){
+    std::vector<int> temp = getTaskIndices(bin_index, nr_of_tasks);
+    std::string result = "";
+    for(unsigned i=0; i<temp.size(); ++i){
+        result += boost::lexical_cast<std::string>(temp[i]);
+        if(i<temp.size()-1){
+            result += "_";
+        }
+    }
+    return result;
+}
+    
+unsigned getNrOfObjectives(unsigned nr_of_bins){
+    unsigned result = 0;
+    nr_of_bins+=1;
+    while(nr_of_bins != 1){
+        nr_of_bins = nr_of_bins >> 1;
+        result+=1;
+    }
+    return result;
 }
 
 } // namespace cmoea
